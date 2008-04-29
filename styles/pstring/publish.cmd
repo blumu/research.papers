6f2d5b797c220db3424714a47a08bf93328323cb
William Blum <william.blum@gmail.com>
2008-04-29 21:07:55 +0000

    git-svn-id: svn://localhost@714 79315eeb-8d0d-0410-b3ce-e36577d3b6d3

--- styles/pstring/publish.cmd
+++
@@ -1,5 +0,0 @@
-copy pstring.zip d:\documents\website\software\latex\
-copy pstring.sty d:\documents\website\software\latex\
-copy pstring-doc.pdf d:\documents\website\software\latex\
-pscp d:\documents\website\software\latex\pstring* william@www.famille-blum.org:public_html/software/latex
-pause
--- styles/pstring/www_publish.cmd
+++
@@ -1,4 +0,0 @@
-pscp pstring.zip william@www.famille-blum.org:public_html/software/latex
-pscp pstring.sty william@www.famille-blum.org:public_html/software/latex
-pscp pstring-doc.pdf william@www.famille-blum.org:public_html/software/latex
-pause
