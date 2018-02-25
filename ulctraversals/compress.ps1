ed6376856ca0942fe235e4ac2b0d65b2ca76d320
William Blum <wiblum@microsoft.com>
2018-02-25 14:36:35 -0800

    squeezed in 39 pages plus appendix

--- ulctraversals/compress.ps1
+++ ulctraversals/compress.ps1
@@ -1,4 +1,12 @@
+param($filebase='ulc-trav')
+
+latexmk -pdf "$filebase.tex" -output-directory=aux-latexmk
+cp aux-latexmk\*.pdf .
+cp aux-latexmk\*.bbl .
+cp aux-latexmk\*.toc .
+Write-Host "Compressing sources..."
 ls *  -File `
 | where { $_.Extension -notin @('.gz', '.zip') }`
 | where { -not $_.Name.StartsWith('notes') } `
-| Compress-Archive -DestinationPath ulctrav.zip -Force
+| Compress-Archive -DestinationPath "$filebase.zip" -Force
+Write-Host "Source file compressed to $filebase.zip"
